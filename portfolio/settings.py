9f69f09c2e9c8bb09beda8f3bbeb8f6a9455a52a
Khadidja02 <dahmaniasma98@gmail.com>
2024-06-25 10:43:09 +0200

    Add whitenoise

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)1d#@up52ac5r6kva2)x4k3wh09pecm!ju8s+k9m=r2!d!dqc$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app", "localhost"]
 
