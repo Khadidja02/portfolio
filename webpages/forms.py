469e7c8245d9ddcead7e76a9e531902d7df77efd
Khadidja02 <dahmaniasma98@gmail.com>
2024-07-01 20:39:02 +0200

    Adding modifs

--- webpages/forms.py
+++ webpages/forms.py
@@ -6,8 +6,8 @@ class ContactForms(forms.ModelForm):
         model = ContactForm
         fields = ['name', 'email', 'subject', 'message']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ton Nom'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ton Email'}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Entrez votre nom'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Entrez votre email'}),
             'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Sujet'}),
             'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Message', 'rows': 5}),
         }
